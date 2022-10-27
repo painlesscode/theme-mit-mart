ab3dc6a4d08ce633b1a561ebfdf02529e350670c
Hazzaz Bin Faiz <hazzazbinfaiz@gmail.com>
2022-10-27 11:53:04 +0600

    Use mix as we are using manifest file

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>{{ config('app.name', 'Laravel') }}</title>
-    <link rel="stylesheet" href="{{ asset('vendor/mit-mart/css/output.css') }}" />
+    <link rel="stylesheet" href="{{ mix('css/output.css', 'vendor/mit-mart') }}" />
 </head>
 
 <body class="font-sans antialiased">
